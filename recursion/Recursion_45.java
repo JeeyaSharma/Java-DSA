f140adf3bb5dc5d513e82d391958e6942a709722
JeeyaSharma <jeeyas39@gmail.com>
2025-11-18 00:12:07 +0530

    new

--- recursion/Recursion_45.java
+++ recursion/Recursion_45.java
@@ -1,24 +1,26 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // Find subsets of an array using recursion
 public class Recursion_45 {
     private static List<List<Integer>> findSubsets(int[] arr){
+        Arrays.sort(arr);
         List<List<Integer>> ans = new ArrayList<>();
         helper(arr, 0, new ArrayList<>(), ans);
         return ans;
     }
     private static void helper(int[] arr, int idx, List<Integer> res, List<List<Integer>> ans){
-        if(idx >= arr.length){
-            ans.add(new ArrayList<>(res));
-            return;
+        ans.add(new ArrayList<>(res));
+        
+        for(int i=idx;i<arr.length;i++){
+            if(i>idx && arr[i]==arr[i-1]) continue;
+            res.add(arr[i]);
+            helper(arr, i+1, res, ans);
+            res.remove(res.size()-1);
         }
-        res.add(arr[idx]);
-        helper(arr, idx+1, res, ans);
-        res.remove(res.size()-1);
-        helper(arr, idx+1, res, ans);
     }
     public static void main(String[] args){
         int[] arr = {1,2,2,3,3,3};
